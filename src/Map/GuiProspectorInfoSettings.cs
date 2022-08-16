7a85677b46477f71327cecb3c82a068ab09fe922
GGGuenni <leonard@eynck.de>
2022-08-16 10:13:30 +0200

    Added "All ores" to heatmapore dropdown
    to be able to deselect the heatmapore

--- src/Map/GuiProspectorInfoSettings.cs
+++ src/Map/GuiProspectorInfoSettings.cs
@@ -11,7 +11,7 @@ namespace ProspectorInfo.Map
         public override string ToggleKeyCombinationCode => "prospectorinfosettings";
         private readonly ModConfig _config;
         private readonly Action<bool> _rebuildMap;
-        private readonly IOrderedEnumerable<KeyValuePair<string, string>> _ores;
+        private readonly List<KeyValuePair<string, string>> _ores;
 
         public GuiProspectorInfoSetting(ICoreClientAPI capi, ModConfig config, Action<bool> rebuildMap) : base(capi)
         {
@@ -20,7 +20,8 @@ namespace ProspectorInfo.Map
 
             IDictionary<string, string> oreValues = Vintagestory.API.Config.Lang.GetAllEntries();
             oreValues.RemoveAll((key, val) => !key.Contains(":ore-") || key.CountChars('-') != 1);
-            _ores = oreValues.OrderBy((pair) => pair.Value);
+            _ores = oreValues.OrderBy((pair) => pair.Value).ToList();
+            _ores.Insert(0, new KeyValuePair<string, string>(null, "All ores"));
 
             SetupDialog();
         }
@@ -39,13 +40,17 @@ namespace ProspectorInfo.Map
             ElementBounds mapModeBounds = ElementBounds.Fixed(35, 100, 120, 20);
             ElementBounds oreBounds = ElementBounds.Fixed(35, 130, 120, 20);
 
+            var currentHeatmapOreIndex = _ores.FindIndex((pair) => pair.Key != null && pair.Key.Contains(_config.HeatMapOre));
+            if (currentHeatmapOreIndex == -1)
+                currentHeatmapOreIndex = 0;
+
             SingleComposer = capi.Gui.CreateCompo("ProspectorInfo Settings", dialogBounds)
                 .AddShadedDialogBG(backgroundBounds)
                 .AddDialogTitleBar("ProspectorInfo", OnCloseTitleBar)
                 .AddStaticText("Show overlay", CairoFont.WhiteDetailText(), showOverlayTextBounds)
                 .AddSwitch(OnSwitchOverlay, switchBounds, "showOverlaySwitch")
                 .AddDropDown(new string[] { "0", "1" }, new string[] { "Default", "Heatmap" }, _config.MapMode, OnMapModeSelected, mapModeBounds)
-                .AddDropDown(_ores.Select((pair) => pair.Key).ToArray(), _ores.Select((pair) => pair.Value).ToArray(), _config.MapMode, OnHeatmapOreSelected, oreBounds)
+                .AddDropDown(_ores.Select((pair) => pair.Key).ToArray(), _ores.Select((pair) => pair.Value).ToArray(), currentHeatmapOreIndex, OnHeatmapOreSelected, oreBounds)
                 .Compose();
 
             SingleComposer.GetSwitch("showOverlaySwitch").On = _config.RenderTexturesOnMap;
@@ -75,7 +80,7 @@ namespace ProspectorInfo.Map
         private void OnHeatmapOreSelected(string code, bool selected)
         {
             // We can assume that only one dash exists in "code" since this a search criteria in the constructor 
-            _config.HeatMapOre = code.Split('-').Last();
+            _config.HeatMapOre = code?.Split('-').Last(); 
             _config.Save(capi);
             _rebuildMap(true);
         }
